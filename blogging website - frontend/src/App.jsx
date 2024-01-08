dae55ecd222be6dc4c6cf7f1cb2bf7cc58f716a3
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2024-01-08 01:14:10 +0100

    git testing

---
+++ blogging website - frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar.component";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>MERN Blogging website by Shakhzodbek</h1>
+        </div>
+    )
+}
+
+export default App;
