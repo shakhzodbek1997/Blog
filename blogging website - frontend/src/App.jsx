6a1d12f6a1f41574897ed5b3f079d51893a3755f
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2024-01-14 19:20:34 +0100

    User Auth UI

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
     return (
-        <div>
-            <Navbar />
-            <h1>MERN Blogging website by Shakhzodbek</h1>
-        </div>
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="signin" element={<UserAuthForm type="sign-in" />}/>
+                <Route path="signup" element={<UserAuthForm type="sign-up" />}/>
+            </Route>
+        </Routes>
     )
 }
 
